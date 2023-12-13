aa1753a9e131eaf724e6d3d336addf5d9a83fe02
Felix Dürrwald <mplabs@mplabs.de>
2023-12-13 08:39:04 +0100

    Day 13

---
+++ 2023/13/index.ts
@@ -0,0 +1,58 @@
+import AbstractPuzzle from '@utils/AbstractPuzzle'
+import { range } from '@utils/array'
+import { equal } from '@utils/utils'
+
+type Grid<T> = T[][]
+
+export default class Day13 extends AbstractPuzzle {
+  get input(): Grid<string>[] {
+    return this.rawInput.split('\n\n').map((block) => block.split('\n').map((row) => row.split('')))
+  }
+
+  public search(grid: Grid<string>) {
+    for (let y = 1; y < grid.length - 1; y++) {
+      const currentRow = grid[y]
+      const previousRow = grid[y - 1]
+
+      if (equal(currentRow, previousRow)) {
+        let valid = true
+
+        for (const delta of range(0, Math.min(y, grid.length - y))) {
+          const nextRow = grid[y + delta]
+          const previousRow = grid[y - 1 - delta]
+
+          if (!equal(nextRow, previousRow)) {
+            valid = false
+            break
+          }
+        }
+
+        if (valid)
+          return {
+            orientation: 'H',
+            position: y,
+          }
+      }
+    }
+  }
+
+  public transpose<T>(grid: Grid<T>): Grid<T> {
+    return grid[0].map((_, i) => grid.map((row) => row[i]))
+  }
+
+  public solveFirst(): unknown {
+    this.input.forEach((grid, idx) => {
+      const result = this.search(grid)
+
+      if (result) {
+        console.log({ [idx]: result })
+      }
+    })
+
+    return null
+  }
+
+  public solveSecond(): unknown {
+    return null
+  }
+}
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,3 +1,30 @@
+export const equal = <T>(a: T, b: T): boolean => {
+  if (typeof a !== typeof b) {
+    return false
+  }
+
+  if (typeof a === 'object') {
+    if (Array.isArray(a) && Array.isArray(b)) {
+      if (a.length !== b.length) {
+        return false
+      }
+
+      return a.every((item, idx) => equal(item, b[idx]))
+    }
+
+    const aKeys = Object.keys(a)
+    const bKeys = Object.keys(b)
+
+    if (aKeys.length !== bKeys.length) {
+      return false
+    }
+
+    return aKeys.every(key => equal(a[key], b[key]))
+  }
+
+  return a === b
+}
+
 export const manhattenDistance = (
   [x1, y1]: [x: number, y: number],
   [x2, y2]: [x: number, y: number]
