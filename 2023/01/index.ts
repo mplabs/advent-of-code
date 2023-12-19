432b3937ad6122faa7393a7e6d435174ffbf0767
Felix Dürrwald <mplabs@mplabs.de>
2023-12-19 12:33:18 +0200

    Day 1 - Part 2

--- 2023/01/index.ts
+++ 2023/01/index.ts
@@ -1,36 +1,5 @@
 import AbstractPuzzle from '@utils/AbstractPuzzle'
 
-export function getDigits(str: string): number[] {
-  return Array.from(str.matchAll(/one|two|three|four|five|six|seven|eight|nine|\d/g))
-    .map((m) => m[0])
-    .map(parseNumber)
-}
-
-export function parseNumber(inStr: string): number {
-  switch (inStr) {
-    case 'one':
-      return 1
-    case 'two':
-      return 2
-    case 'three':
-      return 3
-    case 'four':
-      return 4
-    case 'five':
-      return 5
-    case 'six':
-      return 6
-    case 'seven':
-      return 7
-    case 'eight':
-      return 8
-    case 'nine':
-      return 9
-    default:
-      return parseInt(inStr)
-  }
-}
-
 export default class Day1 extends AbstractPuzzle {
   get input(): string[] {
     return this.rawInput.split('\n')
@@ -45,10 +14,21 @@ export default class Day1 extends AbstractPuzzle {
   }
 
   public solveSecond(): number {
+    const digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    const regExp = new RegExp(`(?=(${digits.join('|')}|\\d))`, 'g')
+
+    const mapDigits = (match: string) => {
+      if (!Number.isNaN(parseInt(match))) {
+        return parseInt(match)
+      }
+
+      return digits.findIndex((d) => d === match) + 1
+    }
+
     return this.input
-      .map((l) => Array.from(l.match(/\d|one|two|three|four|five|six|seven|eight|nine/g)))
+      .map((l) => Array.from(l.matchAll(regExp)).map(m => mapDigits(m[1])))
       .map((l) => [l[0], l[l.length - 1]])
-      .map((l) => 10 * parseNumber(l[0]) + parseNumber(l[1]))
+      .map((l) => 10 * parseInt(l[0]) + parseInt(l[1]))
       .reduce((a, b) => a + b, 0)
   }
 }
