1c2c52475fa3c353c136d275a216c7cef495b7c3
Felix Dürrwald <mplabs@mplabs.de>
2023-12-22 10:27:39 +0200

    cleanup

--- utils/numbers.ts
+++ utils/numbers.ts
@@ -12,3 +12,17 @@ export function inRange(num: number, a: number, b: number): boolean {
 export function hex2dec(hex: string): number {
   return parseInt(hex, 16)
 }
+
+export function wrap(index: number, lowerBound: number, upperBound: number): number {
+  if (lowerBound > upperBound) {
+    throw new Error('Lower bound cannot be greater than upper bound')
+  }
+
+  const rangeSize = upperBound - lowerBound + 1
+
+  if (index < lowerBound) {
+    return upperBound - ((lowerBound - index - 1) % rangeSize)
+  } else {
+    return lowerBound + ((index - lowerBound) % rangeSize)
+  }
+}
