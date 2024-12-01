f18336c7d051027dcf5845d597c13c31139942d2
Felix Dürrwald <mplabs@mplabs.de>
2024-12-01 09:34:39 +0100

    Day 20

---
+++ 2023/20/index.ts
@@ -0,0 +1,13 @@
+
+import AbstractPuzzle from '@utils/AbstractPuzzle'
+
+export default class Day20 extends AbstractPuzzle {
+  public solveFirst(): unknown {
+    return null
+  }
+
+  public solveSecond(): unknown {
+    return null
+  }
+}
+  
