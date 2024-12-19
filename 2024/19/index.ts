d09903257b4f028bf0dc5e11afbdc08d92c1b7cb
Felix Dürrwald <mplabs@mplabs.de>
2024-12-19 10:06:39 +0100

    Day 19 - Part 2

--- 2024/19/index.ts
+++ 2024/19/index.ts
@@ -1,5 +1,5 @@
 import AbstractPuzzle from '@utils/AbstractPuzzle'
-import { Cached } from '@utils/utils'
+import { Cached, memoize } from '@utils/utils'
 
 export default class Day19 extends AbstractPuzzle {
     @Cached()
@@ -20,13 +20,64 @@ export default class Day19 extends AbstractPuzzle {
         return this.input.patterns
     }
 
-    public solveFirst(): unknown {
-        const regExp = new RegExp(`^(${this.available.join('|')})+$`)
+    @Cached()
+    get towels() {
+        return new Set(this.available)
+    }
 
-        return this.patterns.reduce((result, towel) => (towel.match(regExp) ? 1 : 0) + result, 0)
+    @Cached()
+    get maxLength() {
+        return this.available.sort((a, b) => b.length - a.length)[0].length
+    }
+
+    public solveFirst(): unknown {
+        // const regExp = new RegExp(`^(${this.available.join('|')})+$`)
+
+        // return this.patterns.reduce((result, towel) => (towel.match(regExp) ? 1 : 0) + result, 0)
+
+        const check = memoize((pattern: string): number => {
+            if (pattern === '') {
+                return 1
+            }
+            
+            let count = 0, sum = 0
+    
+            for (let i = 1; i <= Math.min(this.maxLength, pattern.length); i++) {
+                let rest = pattern.substring(0, i)
+                if (this.towels.has(rest)) {
+                    count = check(pattern.substring(rest.length))
+                    sum += count
+                    if (count > 0) {
+                        break
+                    }
+                }
+            }
+    
+            return sum
+        })
+
+        return this.patterns.reduce((result, pattern) => result + check(pattern), 0)
     }
 
     public solveSecond(): unknown {
-        return null
+        const check = memoize((pattern: string): number => {
+            if (pattern === '') {
+                return 1
+            }
+            
+            let count = 0, sum = 0
+    
+            for (let i = 1; i <= Math.min(this.maxLength, pattern.length); i++) {
+                let rest = pattern.substring(0, i)
+                if (this.towels.has(rest)) {
+                    count = check(pattern.substring(rest.length))
+                    sum += count
+                }
+            }
+    
+            return sum
+        })
+
+        return this.patterns.reduce((result, pattern) => result + check(pattern), 0)
     }
 }
