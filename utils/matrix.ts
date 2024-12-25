15b7651d1a6e8ee60dabbe9896aa90eb09e200b3
Felix Dürrwald <mplabs@mplabs.de>
2024-12-25 08:26:47 +0100

    Day 25 - Part 1

--- utils/matrix.ts
+++ utils/matrix.ts
@@ -1,3 +1,7 @@
 export function transpose<T>(grid: T[][]): T[][] {
-  return grid[0].map((_, i) => grid.map((row) => row[i]))
+    return grid[0].map((_, i) => grid.map((row) => row[i]))
+}
+
+export function rotate90Clockwise<T>(matrix: T[][]): T[][] {
+    return transpose(matrix).map(row => row.reverse())
 }
